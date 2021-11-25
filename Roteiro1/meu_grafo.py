0caa3d307154643c7648789470f2af145ffe5c50
heitorbrunini <68927093+heitorbrunini@users.noreply.github.com>
2021-11-25 01:35:37 -0300

    conclusão roteiro 1

--- Roteiro1/meu_grafo.py
+++ Roteiro1/meu_grafo.py
@@ -1,5 +1,5 @@
 from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
-from bibgrafo.grafo_exceptions import *
+from bibgrafo.grafo_exceptions import VerticeInvalidoException
 
 
 class MeuGrafo(GrafoListaAdjacencia):
@@ -13,13 +13,29 @@ class MeuGrafo(GrafoListaAdjacencia):
         Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
         :return: Uma lista com os pares de vértices não adjacentes
         '''
-        lista = []
-        aresta = ""
-        for a in self.A:
-            aresta = self.A[a].getV1()+a+self.A[a].getV2()
-            lista.append(aresta)
-        print (lista)
-        print (self.A[0])
+        nao_adjacentes = []
+
+        for i in self.N:
+            adjacentes = []
+
+            for j in self.A:
+                v1 = self.A[j].v1
+                v2 = self.A[j].v2
+                if v1 == i:
+                    adjacentes.append(v2)
+                elif v2 == i:
+                    adjacentes.append(v1)
+            
+            for k in self.N:
+                if k != i and k not in adjacentes:
+                    aresta1 = f'{i}-{k}'
+                    aresta2 = f'{k}-{i}'
+                    if aresta1 not in nao_adjacentes and aresta2 not in nao_adjacentes:
+                        nao_adjacentes.append(f'{i}-{k}')
+
+            adjacentes = []
+
+        return nao_adjacentes
 
     def ha_laco(self):
         '''
@@ -75,13 +91,40 @@ class MeuGrafo(GrafoListaAdjacencia):
         :return: Uma lista os rótulos das arestas que incidem sobre o vértice
         :raises: VerticeInvalidoException se o vértice não existe no grafo
         '''
-        pass
+        arestas = []
+        ha_vertice = False
+
+        for i in self.N:
+            if i == V:
+                ha_vertice = True
+
+        if not ha_vertice:
+            raise VerticeInvalidoException("Não existe um vértice" , V, " no grafo.")
+
+        for i in self.A:
+            if self.A[i].v1 == V or self.A[i].v2 == V:
+                arestas.append(self.A[i].rotulo)
+
+        return arestas
 
     def eh_completo(self):
         '''
         Verifica se o grafo é completo.
         :return: Um valor booleano que indica se o grafo é completo
         '''
-        pass
+        if self.ha_laco():
+            return False
+
+        if self.ha_paralelas():
+            return False
+
+        numV = len(self.N)
+
+        for i in self.N:
+            grau = self.grau(i)
+
+            if grau != numV - 1:
+                return False 
+        return True
     
 
